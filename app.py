a1789944147be09306c6827c1a820192493ddfc0
loganmurphy <loganmurphy1984@gmail.com>
2017-10-10 16:31:17 -0500

    test

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plot
 from PIL import Image
 import numpy as np
 
+import urllib
+
 import requests
 import json
 
@@ -30,9 +32,35 @@ class TemplateHandler(tornado.web.RequestHandler):
 class MainHandler(TemplateHandler):
   def get (self):
     # render input form
-    self.render_template('home.html', {})
+    x_real_ip = self.request.headers.get("X-Real-IP")
+    remote_ip = x_real_ip or self.request.remote_ip
+    print(remote_ip)
+    if remote_ip == '::1':
+        url = 'https://ipinfo.io/json'
+        response = requests.get(url)
+        print(response.json())
+    else:
+        url = 'https://ipinfo.io/{}/json'.format(remote_ip)
+    # self.render_template('home.html', {})
+    city = response.json()['city']
+    weather = Weather.select().where(Weather.city == city).get()
+    print(city)
+    self.render_template('weather.html', {'weather': weather, 'city': city})
 
   def post (self):
+    #  Write a function for this.
+    city_check = ['Houston', 'Taipei', 'San Francisco']
+    for city in city_check:
+        url = "http://api.openweathermap.org/data/2.5/weather"
+        headers = {
+            'cache-control': "no-cache",
+            'postman-token': "fe66220c-7377-25b8-1688-3c5552c5eaef"
+            }
+        querystring = {"APPID":"5fadb7bdf915f1e0ef22880fb806b684","q": city }
+        response = requests.request("POST", url, headers=headers, params=querystring)
+        weather_update = Weather.create(city=city, weather_data=response.json())
+    print("Updated")
+
     city = self.get_body_argument('city')
     url = "http://api.openweathermap.org/data/2.5/weather"
     old = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
@@ -54,7 +82,7 @@ class MainHandler(TemplateHandler):
         response = requests.request("POST", url, headers=headers, params=querystring)
         weather = Weather.create(city=city, weather_data=response.json())
     print(weather, weather.created)
-    self.render_template('weather.html', {"weather": weather, 'city': city})
+    self.render_template('weather.html', {'weather': weather, 'city': city})
 
 class PlotGraph(TemplateHandler):
   def get (self, history):
@@ -62,46 +90,43 @@ class PlotGraph(TemplateHandler):
     city = self.get_query_argument('city')
     print(city)
     weather = Weather.select().where(Weather.city == city).order_by(Weather.created.desc()).limit(5)
-    pics = ['sm_clear.png', 'sm_clouds.png', 'sm_fog.png', 'sm_rain.png', 'sm_snow.png', 'sm_storm.png']
-    im = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[0])
-    im2 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[1])
-    im3 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[2])
-    im4 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[3])
-    im5 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[4])
-    im6 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/' + pics[5])
-
-    weather_pic = []
+    your_city = Weather.city
+    im1 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_clear.png')
+    im2 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_clouds.png')
+    im3 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_fog.png')
+    im4 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_rain.png')
+    im5 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_snow.png')
+    im6 = Image.open('/Users/loganmurphy/desktop/weather_app/static/img/sm_storm.png')
+    weather_pic = ' '
     temp = []
     xs = [1, 2, 3, 4, 5]
+
     i = 0
     while i < 5:
-    #   weather_pic.append(weather[i].weather_data['weather'][0]['main'])
       temp_in_celcius = (weather[i].weather_data['main']['temp'] - 273.15)
       temp.append(temp_in_celcius)
       if weather[i].weather_data['weather'][0]['main'] in ['Rain','Drizzle']:
-          weather_pic.append(im)
+          weather_pic = im4
       elif weather[i].weather_data['weather'][0]['main'] in ['Mist', 'Haze', 'Smoke']:
-          weather_pic.append(im)
+          weather_pic = im3
       elif weather[i].weather_data['weather'][0]['main'] == 'Clouds':
-          weather_pic.append(im)
+          weather_pic = im2
       elif weather[i].weather_data['weather'][0]['main'] == 'Clear':
-          weather_pic.append(im)
+          weather_pic = im1
       elif weather[i].weather_data['weather'][0]['main'] == 'Snow':
-          weather_pic.append(im)
-      elif weather[i].weather_data['weather'][0]['main'] == 'storm':
-          weather_pic.append(im)
+          weather_pic = im5
+      elif weather[i].weather_data['weather'][0]['main'] == 'Storm':
+          weather_pic = im6
       i += 1
-    print(weather_pic)
-    self.write("OK2")
+
     fig = plot.figure()
-    fig.figimage(im, 122.5, fig.bbox.ymin + 10)
-    fig.figimage(im, 215.5, fig.bbox.ymin + 10)
-    fig.figimage(im, 310.5, fig.bbox.ymin + 10)
-    fig.figimage(im, 402.5, fig.bbox.ymin + 10)
-    fig.figimage(im, 500.5, fig.bbox.ymin + 10)
+    fig.figimage(weather_pic, 122.5, fig.bbox.ymin + 10)
+    fig.figimage(weather_pic, 215.5, fig.bbox.ymin + 10)
+    fig.figimage(weather_pic, 310.5, fig.bbox.ymin + 10)
+    fig.figimage(weather_pic, 402.5, fig.bbox.ymin + 10)
+    fig.figimage(weather_pic, 500.5, fig.bbox.ymin + 10)
     plot.bar(xs, temp)
     plot.ylabel('Temperature in °C')
-    # plot.xlabel('Recent Measurements')
     plot.savefig('static/img/history.png', dpi=None, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format='png',
         transparent=False, bbox_inches=None, pad_inches=0.1,
