6c6ce9a8c35c59bb6ae92f63bcdca5e8a0bee040
loganmurphy <loganmurphy1984@gmail.com>
2017-10-14 22:57:02 -0500

    updated api call

--- weather_app.py
+++ weather_app.py
@@ -53,18 +53,18 @@ class MainHandler(TemplateHandler):
     else:
         url = 'https://ipinfo.io/{}/json'.format(remote_ip)
     self.render_template('home.html', {})
-    city = ''
-    url = "http://api.openweathermap.org/data/2.5/weather"
-    querystring = {"APPID":"5fadb7bdf915f1e0ef22880fb806b684","q": city}
-    headers = {
-        'cache-control': "no-cache",
-        'postman-token': "fe66220c-7377-25b8-1688-3c5552c5eaef"
-        }
-    response = requests.request("POST", url, headers=headers, params=querystring)
-    weather = Weather.create(city=city, weather_data=response.json())
-
-    weather = Weather.select().where(Weather.city == city).get()
-    print(city)
+    # city = ''
+    # url = "http://api.openweathermap.org/data/2.5/weather"
+    # querystring = {"APPID":"5fadb7bdf915f1e0ef22880fb806b684","q": city}
+    # headers = {
+    #     'cache-control': "no-cache",
+    #     'postman-token': "fe66220c-7377-25b8-1688-3c5552c5eaef"
+    #     }
+    # response = requests.request("POST", url, headers=headers, params=querystring)
+    # weather = Weather.create(city=city, weather_data=response.json())
+    #
+    # weather = Weather.select().where(Weather.city == city).get()
+    # print(city)
     # self.render_template('weather.html', {'weather': weather, 'city': city})
 
   def post (self):
